63b2d65a7a895da1c73aac3ebfa35e996bba565a
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2021-02-28 21:37:19 +0100

    front et back

---
+++ src/Entity/Chauffeur.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Chauffeur
+ *
+ * @ORM\Table(name="chauffeur")
+ * @ORM\Entity
+ */
+class Chauffeur
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_chauffeur", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idChauffeur;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="prenom", type="string", length=255, nullable=true)
+     */
+    private $prenom;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="date_d_n", type="date", nullable=true)
+     */
+    private $dateDN;
+
+    public function getIdChauffeur(): ?int
+    {
+        return $this->idChauffeur;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getDateDN(): ?\DateTimeInterface
+    {
+        return $this->dateDN;
+    }
+
+    public function setDateDN(?\DateTimeInterface $dateDN): self
+    {
+        $this->dateDN = $dateDN;
+
+        return $this;
+    }
+
+
+}
