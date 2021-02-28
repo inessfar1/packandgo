2ba4ad4a7761be02aa32e1f4f6e3b4f1268457cc
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2021-02-28 23:48:03 +0100

    Affichage

--- src/Controller/AgenceController.php
+++ src/Controller/AgenceController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\AgenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class AgenceController extends AbstractController
 {
@@ -17,4 +19,16 @@ class AgenceController extends AbstractController
             'controller_name' => 'AgenceController',
         ]);
     }
-}
+
+
+/**
+ * @param AgenceRepository $repository
+ * @return mixed
+ * @Route("Back/Agence",name="AgenceAff")
+ */
+public function afficheAgence(AgenceRepository $repository){
+    $agence=$repository->findAll();
+    return $this->render('agence/afficheAgenceBack.html.twig', [
+        'agence' => $agence
+    ]);
+}}
