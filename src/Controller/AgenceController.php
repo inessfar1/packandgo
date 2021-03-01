1f423bf0b74e6322c68b1f3d55b0367730ac68c6
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2021-03-01 01:41:06 +0100

    Ajout agence back

--- src/Controller/AgenceController.php
+++ src/Controller/AgenceController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Agence;
+use App\Form\AgenceType;
 use App\Repository\AgenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class AgenceController extends AbstractController
@@ -43,6 +47,26 @@ public function afficheAgence(AgenceRepository $repository){
         $em->flush();
 
         return $this->redirectToRoute('AgenceAff');
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("Back/AgenceAjout" ,name="AgenceAdd")
+     */
+    public function ajouterAgence(Request $request){
+        $agence=new Agence();
+        $form=$this->createForm(AgenceType::class,$agence);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($agence);
+            $em->flush();
+            return $this->redirectToRoute('AgenceAff');
+
+        }
+        return $this->render('agence/ajouterAgence.html.twig',['form'=>$form->createView()]);
     }
 
 }
--- src/Form/AgenceType.php
+++ src/Form/AgenceType.php
@@ -4,7 +4,13 @@ namespace App\Form;
 
 use App\Entity\Agence;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AgenceType extends AbstractType
@@ -12,12 +18,52 @@ class AgenceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('pays')
-            ->add('ville')
-            ->add('adresse')
-            ->add('email')
-            ->add('tel')
+            ->add('nom',TextType::class,[
+                'label'=>'Nom Agence',
+                'attr'=>[
+                    'placeholder'=>'Entrer le nom ici',
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('pays',TextType::class,[
+                'label'=>'Pays',
+                'attr'=>[
+                    'placeholder'=>'Entrer le pays ici',
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('ville',TextType::class,[
+                'label'=>'Ville',
+                'attr'=>[
+                    'placeholder'=>'Entrer la ville ici',
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('adresse',TextareaType::class,[
+                'label'=>'Addresse',
+                'attr'=>[
+                    'placeholder'=>'Entrer adresse ici',
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('email',EmailType::class,[
+                'label'=>'Email',
+                'attr'=>[
+                    'placeholder'=>'Entrer email ici',
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('tel',TelType::class,[
+                'label'=>'Tel',
+                'attr'=>[
+                    'placeholder'=>'Entrer num tel ici',
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('Ajouter',SubmitType::class,[
+                'attr'=>[
+        'class'=>'btn btn-primary']]
+    )
         ;
     }
 
