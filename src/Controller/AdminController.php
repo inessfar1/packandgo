7523baa100afeef641ec78527e2b258cbf99f8f9
hedi driss <hdriss8@gmail.com>
2021-03-05 01:40:59 +0100

    commit 1
    CRUD et controle de saisier pour les deux tables

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\LoginType;
+use App\Form\UtildashType;
+use App\Form\UtilisateurType;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request ;
+use Doctrine\Persistence\ManagerRegistry ;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="global")
+     */
+    public function index(): Response
+    {
+
+        return $this->render('admin/home.html.twig') ;
+
+    }
+
+    /**
+     * @Route("/user/{id}", name="globaluser")
+     */
+    public function indexuser(UtilisateurRepository $repository,$id): Response
+    {
+        $utilisateur = $repository->find($id) ;
+        return $this->render('admin/home.html.twig',[
+            "utilisateur"=>$id
+        ]) ;
+    }
+
+    /**
+     * @Route("/utilisateur/creation", name="utilisateur_creation")
+     * @Route("/utilisateur/{id}", name="utilisateur_modification",methods="GET|POST")
+     */
+    public function ajouETmodif(Utilisateur $utilisateur = null, Request $request , ManagerRegistry $objectManager , UserPasswordEncoderInterface $encoder): Response
+    {
+        if(!$utilisateur)
+        {
+            $utilisateur = new Utilisateur() ;
+        }
+
+        $form = $this->createForm(UtilisateurType::class,$utilisateur) ; //bech twarik les champ l moujoudin
+        $form->handleRequest($request) ;  //recuperer les valeur
+        if($form->isSubmitted() && $form->isValid()){ //verifier le formulaire
+            $passwordCrypte = $encoder->encodePassword($utilisateur,$utilisateur->getPassword()) ;
+            $utilisateur->setPassword($passwordCrypte) ;
+            $utilisateur->setRoles("ROLE_USER") ;
+            $modif =  $utilisateur->getId() !== null ;
+            $em = $objectManager->getManager();
+            $em->persist($utilisateur); //prepaer (tekhou les valeur)
+            $em->flush(); //aaporter les modification f base
+            $this->addFlash("success",($modif) ? "La modification a ete effectuee":"l'ajout a ete effectuee") ;
+            return $this->redirectToRoute("global") ;
+        }
+
+
+
+        return $this->render('admin/formulaire.html.twig',[
+            "utilisateur" => $utilisateur,
+            "form"=>$form->createView(),
+            "isModification" => $utilisateur->getId() !== null // si element mouch moujoud trajaa false
+        ]);
+    }
+
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $util)
+    {
+
+        return $this->render('admin/index.html.twig',[
+            "lastUserName"=>$util->getLastUsername(),
+            "error"=>$util->getLastAuthenticationError()
+        ]);
+
+    }
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+    }
+
+    /**
+     * @Route("/admin/global", name="back")
+     */
+    public function aff(): Response
+    {
+        return $this->render('dashboard.html.twig');
+    }
+    /**
+     * @Route("/admin/affichage", name="util")
+     */
+    public function affichage(UtilisateurRepository $repository): Response
+    {
+        $utilisateur= $repository->findAll() ;
+        return $this->render('backend/utilisateur.html.twig', [
+            'util' => $utilisateur,
+
+        ]);
+    }
+
+    /**
+     * @Route("/admin/util/creation", name="admin_creation")
+     * @Route("/admin/util/{id}", name="admin_modification",methods="GET|POST")
+     */
+    public function ajoutetmodifdash(Utilisateur $utilisateur = null, Request $request , ManagerRegistry $objectManager , UserPasswordEncoderInterface $encoder): Response
+    {
+        if(!$utilisateur)
+        {
+            $utilisateur = new Utilisateur() ;
+        }
+
+        $form = $this->createForm(UtildashType::class,$utilisateur) ; //bech twarik les champ l moujoudin
+        $form->handleRequest($request) ;  //recuperer les valeur
+        if($form->isSubmitted() && $form->isValid()){ //verifier le formulaire
+            $passwordCrypte = $encoder->encodePassword($utilisateur,$utilisateur->getPassword()) ;
+            $utilisateur->setPassword($passwordCrypte) ;
+            $modif =  $utilisateur->getId() !== null ;
+            $em = $objectManager->getManager();
+            $em->persist($utilisateur); //prepaer (tekhou les valeur)
+            $em->flush(); //aaporter les modification f base
+            $this->addFlash("success",($modif) ? "La modification a ete effectuee":"l'ajout a ete effectuee") ;
+            return $this->redirectToRoute("util") ;
+        }
+
+
+
+        return $this->render('backend/modification.html.twig',[
+            "utilisateur" => $utilisateur,
+            "form"=>$form->createView(),
+            "isModification" => $utilisateur->getId() !== null // si element mouch moujoud trajaa false
+        ]);
+    }
+
+    /**
+     * @Route("/admin/util/{id}", name="admin_suppression",methods="delete")
+     */
+    public function suppression(Utilisateur $utilisateur, Request $request , ManagerRegistry $objectManager ): Response
+    {
+        if($this->isCsrfTokenValid("SUP". $utilisateur->getId(),$request->get('_token'))){
+            $em = $objectManager->getManager();
+            $em->remove($utilisateur);
+            $em->flush();
+            $this->addFlash("success","La suppression a ete effectuee") ;
+            return $this->redirectToRoute("util") ;
+        }
+
+    }
+
+    /**
+     * @Route("/profil", name="profil")
+     */
+    public function affichageprofil(): Response
+    {
+
+        return $this->render('admin/profil.html.twig');
+    }
+
+}
---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert ;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity ;
+
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateurRepository::class)
+ * @Vich\Uploadable
+ * @UniqueEntity(
+ *     fields = {"username"},
+ *     message="le username existe deja"
+ * )
+ */
+class Utilisateur implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="utilisateur_image", fileNameProperty="image")
+     */
+    private $imageFile ;
+
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @Assert\Length(min=5,max=10,minMessage="il faut au moin 5 carac",maxMessage="il faut au max 10 carac")
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @Assert\Length(min=5,max=10,minMessage="il faut au moin 5 carac",maxMessage="il faut au max 10 carac")
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @Assert\EqualTo(propertyPath="password",message="les mdp ne correspondent pas")
+     */
+    private $verifpassword ;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $roles;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Reclamation::class, mappedBy="utilisateurs" , orphanRemoval=true)
+     */
+    private $reclamations;
+
+    public function __construct()
+    {
+        $this->reclamations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getVerifpassword(): ?string
+    {
+        return $this->verifpassword;
+    }
+
+    public function setVerifpassword(string $verifpassword): self
+    {
+        $this->verifpassword = $verifpassword;
+
+        return $this;
+    }
+
+    public function getRoles()
+    {
+        return [$this->roles];
+    }
+
+    public function setRoles(?string $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+
+    public function setImageFile(?File $imageFile = null): self
+    {
+        $this->imageFile = $imageFile;
+        return $this ;
+
+        //if (null !== $imageFile) {
+        // It is required that at least one field changes if you are using doctrine
+        // otherwise the event listeners won't be called and the file is lost
+        //$this->updatedAt = new \DateTimeImmutable();
+    }
+
+    /**
+     * @return Collection|Reclamation[]
+     */
+    public function getReclamations(): Collection
+    {
+        return $this->reclamations;
+    }
+
+    public function addReclamation(Reclamation $reclamation): self
+    {
+        if (!$this->reclamations->contains($reclamation)) {
+            $this->reclamations[] = $reclamation;
+            $reclamation->setUtilisateurs($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReclamation(Reclamation $reclamation): self
+    {
+        if ($this->reclamations->removeElement($reclamation)) {
+            // set the owning side to null (unless already changed)
+            if ($reclamation->getUtilisateurs() === $this) {
+                $reclamation->setUtilisateurs(null);
+            }
+        }
+
+        return $this;
+    }
+}
